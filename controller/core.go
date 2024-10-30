685151991a50435f4babf30f6690a3f360d7375f
opensuse156 <westroads@gmail.com>
2024-10-30 05:00:22 +0300

    fix template for fold

--- controller/core.go
+++ controller/core.go
@@ -101,10 +101,10 @@ func GetAll(c *fiber.Ctx) error {
                             <a href="` + filepath.Join(c_path, e.Name()) + `" class="list-group-item list-group-item-action fold">
                                 <div class="d-flex w-100 justify-content-between">
                                     <h5 class="mb-1"><i class="bi bi-folder"></i> ` + e.Name() + `</h5>
-                                    <small class="text-muted">` + size_human + ` </small>
+                                    <!--small class="text-muted">` + size_human + ` </small-->
                                 </div>
                                 <!--p class="mb-1">Some placeholder content in a paragraph.</p-->
-                                <small class="text-muted">` + modtime_human + `</small>
+                                <!--small class="text-muted">` + modtime_human + `</small-->
                             </a>
 
                         `
