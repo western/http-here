17bc2e3f263d1d7f25c28a59cff7432846b2137f
opensuse156 <westroads@gmail.com>
2024-10-31 14:33:26 +0300

    fix another 500

--- controller/core.go
+++ controller/core.go
@@ -45,7 +45,7 @@ func GetAll(c *fiber.Ctx) error {
 
 		log.Println(err)
 		LogPrefix(c, "500", "Error "+filepath.Join(arg_fold, c_path))
-		return c.Status(fiber.StatusNotFound).Render("view/500", fiber.Map{}, "view/layout")
+		return c.Status(fiber.StatusInternalServerError).Render("view/500", fiber.Map{}, "view/layout")
 	}
 
 	c_path = CleanDirtyPath(c_path)
