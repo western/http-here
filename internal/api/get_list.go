6dbac4bdd8804ffd01e8725ea623e7a9e4e72a7e
opensuse156 <westroads@gmail.com>
2025-06-26 23:24:36 +0300

    api/get_list: changes for generateRows call

--- internal/api/get_list.go
+++ internal/api/get_list.go
@@ -26,26 +26,17 @@ func GetList(c *fiber.Ctx) error {
 
 	u_path = util.CleanDirtyPath(u_path)
 
+	readFolder := path.Join(arg_fold, u_path)
+
 	// -------------------------------------------------------------------------------------------------------------------------
 
-	if _, err := os.Stat(path.Join(arg_fold, u_path)); err != nil {
+	if _, err := os.Stat(readFolder); err != nil {
 
-		model.EventLogAdd(db, c, "500", "GetList", "'"+path.Join(arg_fold, u_path)+"' not exists")
+		model.EventLogAdd(db, c, "500", "GetList", "'"+readFolder+"' not exists")
 
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"code": 500,
 			"msg":  path.Join(u_path) + " not exists",
-		}, "application/json")
-	}
-
-	entries, err := os.ReadDir(path.Join(arg_fold, u_path))
-	if err != nil {
-
-		model.EventLogAdd(db, c, "500", "GetList", "Error "+path.Join(arg_fold, u_path)+" "+err.Error())
-
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"code": 500,
-			"msg":  path.Join(u_path) + " readdir error",
 		}, "application/json")
 	}
 
@@ -56,9 +47,28 @@ func GetList(c *fiber.Ctx) error {
 		s_sort = "name"
 	}
 
-	rows := listGenerateView(db, arg_fold, u_path, entries, s_sort)
+	rows, err := generateRows(db, c, s_sort)
+	if err != nil {
 
-	model.EventLogAdd(db, c, "200", "GetList", "Get list '"+path.Join(arg_fold, u_path)+"'")
+		if os.IsPermission(err) {
+
+			model.EventLogAdd(db, c, "403", "GetList", "Forbidden for read "+readFolder)
+
+			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"code": 403,
+				"msg":  "Forbidden",
+			}, "application/json")
+		}
+
+		model.EventLogAdd(db, c, "500", "GetList", "generateRows err: "+err.Error())
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": 500,
+			"msg":  "error",
+		}, "application/json")
+	}
+
+	model.EventLogAdd(db, c, "200", "GetList", "Get list '"+readFolder+"'")
 
 	return c.JSON(fiber.Map{
 		"code": 200,
