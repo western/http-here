3f124a9e2f43eeae5537f7acb2c2226ea90e8be0
opensuse156 <westroads@gmail.com>
2025-06-30 01:11:02 +0300

    api/post_move: prepare for clipboard mode

--- internal/api/post_move.go
+++ internal/api/post_move.go
@@ -1,11 +1,10 @@
 package api
 
 import (
-	"fmt"
-	"net/url"
+	//"fmt"
+	//"net/url"
 	"os"
 	"path"
-	_ "path/filepath"
 	"strings"
 
 	"github.com/western/http-here/internal/model"
@@ -23,69 +22,69 @@ func PostMove(c *fiber.Ctx) error {
 
 	prefix := c.Locals("prefix").(string)
 
-	referer := c.Get("Referer")
-
 	db := c.Locals("db").(*gorm.DB)
-
-	// already decoded
-	u, err := url.Parse(referer)
-	if err != nil {
-
-		model.EventLogAdd(db, c, "500", "PostMove", "Error url parse "+referer+" "+err.Error())
-
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"code": 500,
-			"msg":  "Error url parse " + referer,
-		}, "application/json")
-	}
-
-	u_path := util.CleanDirtyPath(u.Path)
 
 	// -------------------------------------------------------------------------------------------------------------------------
 
 	from_path := c.FormValue("from_path")
 	from_path = util.CleanDirtyPath(from_path)
 
-	// -------------------------------------------------------------------------------------------------------------------------
+	if len(from_path) == 0 {
 
-	to := c.FormValue("to")
-	to = util.CleanDirtyPath(to)
-
-	if len(to) == 0 {
-
-		model.EventLogAdd(db, c, "500", "PostMove", "to is empty")
+		model.EventLogAdd(db, c, "500", "PostMove", "from_path is empty")
 
 		return c.JSON(fiber.Map{
 			"code": 500,
-			"msg":  "to is empty",
+			"msg":  "from_path is empty",
 		}, "application/json")
 	}
 
-	_, err = os.Stat(path.Join(arg_fold, to))
+	_, err := os.Stat(path.Join(arg_fold, from_path))
 	if err != nil {
 
-		model.EventLogAdd(db, c, "500", "PostMove", "'"+path.Join(arg_fold, to)+"' not exists "+err.Error())
+		model.EventLogAdd(db, c, "500", "PostMove", "'"+path.Join(arg_fold, from_path)+"' not exists "+err.Error())
 
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"code": 500,
-			"msg":  "'" + path.Join(arg_fold, to) + "' not exists",
+			"msg":  "'" + path.Join(from_path) + "' not exists",
 		}, "application/json")
 	}
 
 	// -------------------------------------------------------------------------------------------------------------------------
 
-	src_file_path := u_path
-	if len(from_path) > 0 {
-		src_file_path = from_path
+	to_path := c.FormValue("to_path")
+	to_path = util.CleanDirtyPath(to_path)
+
+	if len(to_path) == 0 {
+
+		model.EventLogAdd(db, c, "500", "PostMove", "to_path is empty")
+
+		return c.JSON(fiber.Map{
+			"code": 500,
+			"msg":  "to_path is empty",
+		}, "application/json")
 	}
 
-	if src_file_path == to {
+	_, err = os.Stat(path.Join(arg_fold, to_path))
+	if err != nil {
 
-		model.EventLogAdd(db, c, "500", "PostMove", "Source and target paths '"+to+"' are identical")
+		model.EventLogAdd(db, c, "500", "PostMove", "'"+path.Join(arg_fold, to_path)+"' not exists "+err.Error())
 
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"code": 500,
-			"msg":  "Source and target paths '" + to + "' are identical",
+			"msg":  "'" + path.Join(to_path) + "' not exists",
+		}, "application/json")
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------------
+
+	if from_path == to_path {
+
+		model.EventLogAdd(db, c, "500", "PostMove", "Source and target paths '"+from_path+"' are identical")
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": 500,
+			"msg":  "Source and target paths '" + from_path + "' are identical",
 		}, "application/json")
 	}
 
@@ -106,67 +105,74 @@ func PostMove(c *fiber.Ctx) error {
 
 	var err_list []string
 
-	for _, val := range names {
+	for _, name := range names {
 
-		if len(val) > 0 {
+		name := strings.ReplaceAll(name, "/", "")
+		name = util.CleanDirtyPath(name)
 
-			name := strings.ReplaceAll(val, "/", "")
-			//re := regexp.MustCompile("\\s+")
-			//name = re.ReplaceAllLiteralString(name, " ")
-
-			name = util.CleanDirtyPath(name)
-
-			if len(name) == 0 {
-
-				model.EventLogAdd(db, c, "500", "PostMove", "name is empty")
-				err_list = append(err_list, "name is empty")
-				continue
-			}
-
-			src_file_path := path.Join(arg_fold, u_path, name)
-			if len(from_path) > 0 {
-				src_file_path = path.Join(arg_fold, from_path, name)
-			}
-
-			target_file_path := path.Join(arg_fold, to, name)
-
-			_, err := os.Stat(src_file_path)
-			if err != nil {
-
-				model.EventLogAdd(db, c, "500", "PostMove", "Source '"+src_file_path+"' not exists")
-				err_list = append(err_list, "Source '"+src_file_path+"' not exists")
-				continue
-			}
-
-			_, err = os.Stat(target_file_path)
-			if err == nil {
-
-				model.EventLogAdd(db, c, "200", "PostMove", "Target '"+target_file_path+"' is exists. It will be rewrite.")
-
-				if err := os.RemoveAll(target_file_path); err != nil {
-
-					model.EventLogAdd(db, c, "500", "PostMove", "'"+target_file_path+"' err "+err.Error())
-					err_list = append(err_list, "'"+target_file_path+"' err "+err.Error())
-					continue
-				}
-			}
-
-			err = os.Rename(src_file_path, target_file_path)
-
-			if err != nil {
-
-				model.EventLogAdd(db, c, "500", "PostMove", "Rename error "+fmt.Sprintf("%s", err))
-				err_list = append(err_list, "Rename error "+fmt.Sprintf("%s", err))
-
-			} else {
-
-				model.EventLogAdd(db, c, "200", "PostMove", "Move '"+src_file_path+"' to "+target_file_path)
-			}
-
+		if len(name) == 0 {
+			model.EventLogAdd(db, c, "500", "PostMove", "name is empty")
+			err_list = append(err_list, "name is empty")
+			continue
 		}
+
+		src_file_path := path.Join(arg_fold, from_path, name)
+		target_file_path := path.Join(arg_fold, to_path, name)
+
+		src_stat, err := os.Stat(src_file_path)
+		if err != nil {
+			model.EventLogAdd(db, c, "500", "PostMove", "Source '"+src_file_path+"' not exists")
+			err_list = append(err_list, "Source '"+src_file_path+"' not exists")
+			continue
+		}
+
+		_, err = os.Stat(target_file_path)
+		if err == nil {
+
+			model.EventLogAdd(db, c, "200", "PostMove", "Target '"+target_file_path+"' is exists. It will be rewrite.")
+
+			if err := os.RemoveAll(target_file_path); err != nil {
+				model.EventLogAdd(db, c, "500", "PostMove", "'"+target_file_path+"' err "+err.Error())
+				err_list = append(err_list, "'"+target_file_path+"' err "+err.Error())
+				continue
+			}
+		}
+
+		if src_stat.IsDir() {
+			err := util.CopyDir(src_file_path, target_file_path)
+
+			if err != nil {
+				model.EventLogAdd(db, c, "500", "PostMove", "CopyDir '"+src_file_path+"' to '"+target_file_path+"' err "+err.Error())
+				err_list = append(err_list, "CopyDir '"+src_file_path+"' to '"+target_file_path+"' err "+err.Error())
+				continue
+			}
+
+			// remove source folder
+			if err := os.RemoveAll(src_file_path); err != nil {
+				model.EventLogAdd(db, c, "500", "PostMove", "rm folder '"+src_file_path+"' err "+err.Error())
+				err_list = append(err_list, "'"+src_file_path+"' err "+err.Error())
+				continue
+			}
+
+			model.EventLogAdd(db, c, "200", "PostMove", "Move dir '"+src_file_path+"' to "+target_file_path)
+
+		} else {
+			err := util.MoveFile(src_file_path, target_file_path)
+
+			if err != nil {
+				model.EventLogAdd(db, c, "500", "PostMove", "MoveFile '"+src_file_path+"' to '"+target_file_path+"' err "+err.Error())
+				err_list = append(err_list, "MoveFile '"+src_file_path+"' to '"+target_file_path+"' err "+err.Error())
+				continue
+			}
+
+			model.EventLogAdd(db, c, "200", "PostMove", "Move file '"+src_file_path+"' to "+target_file_path)
+		}
+
 	}
 
 	go model.FileChkAsync(db, prefix)
+	RemoveCacheDir(path.Join(arg_fold, from_path))
+	RemoveCacheDir(path.Join(arg_fold, to_path))
 
 	if len(err_list) > 0 {
 
