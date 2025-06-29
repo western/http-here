eee28b2c26ae9a1b189bd8eb2f500deee4aaa948
opensuse156 <westroads@gmail.com>
2025-06-30 01:23:24 +0300

    search: add arg_usedb for show warning

--- internal/app/search.go
+++ internal/app/search.go
@@ -13,6 +13,11 @@ func GetSearch(c *fiber.Ctx) error {
 
 	db := c.Locals("db").(*gorm.DB)
 
+	arg_usedb := ""
+	if c.Locals("arg_usedb") != nil {
+		arg_usedb = c.Locals("arg_usedb").(string)
+	}
+
 	arg_fold := ""
 	arg_fold = c.Locals("arg_fold").(string)
 
@@ -22,9 +27,14 @@ func GetSearch(c *fiber.Ctx) error {
 
 	model.EventLogAdd(db, c, "200", "GetSearch", "Get search '"+s+"'")
 
+	if db == nil {
+		model.EventLogAdd(db, c, "200", "GetSearch", "You should enable database for search")
+	}
+
 	return c.Render("view/search", fiber.Map{
 
 		"s":           s,
 		"result_list": result_list,
+		"arg_usedb":   arg_usedb,
 	}, "view/layout/default")
 }
