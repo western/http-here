a0e3d81d97206e8dd46203d0520bee41e1ca7ef9
opensuse156 <westroads@gmail.com>
2026-03-05 15:24:17 +0300

    modified:   internal/app/thumb.go

--- internal/app/thumb.go
+++ internal/app/thumb.go
@@ -32,6 +32,12 @@ import (
 	"math"
 )
 
+var (
+	previewImgRegex = regexp.MustCompile("^(jpg|jpeg|png|gif)$")
+	previewDocRegex = regexp.MustCompile("^(pdf|rtf|doc|docx|xls|xlsx|odt|ods)$")
+	previewAllRegex = regexp.MustCompile("^(jpg|jpeg|png|gif|pdf|rtf|doc|docx|xls|xlsx|odt|ods)$")
+)
+
 func GetThumb(c *fiber.Ctx) error {
 
 	if _, err := os.Stat(path.Join(conf.ConfigRoot, "thumb")); err != nil {
@@ -63,19 +69,18 @@ func GetThumb(c *fiber.Ctx) error {
 
 	// ------------------------------------------------------------------------------------------------------------------------------
 
-	i_width := 600
-
-	fileInfo, err := os.Stat(path.Join(conf.ArgFold, c_path))
+	fileInfoOriginal, err := os.Stat(path.Join(conf.ArgFold, c_path))
+	fileModeOriginal := fileInfoOriginal.Mode()
 
 	if err == nil {
 
-		if fileInfo.IsDir() {
+		if !fileModeOriginal.IsRegular() {
 
-			model2.EventLogAdd(c, 500, "GetThumb", "Error "+path.Join(conf.ArgFold, c_path)+" It is a folder")
+			model2.EventLogAdd(c, 500, "GetThumb", "Error "+path.Join(conf.ArgFold, c_path)+" It is not a regular file")
 
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 				"code": 500,
-				"file": path.Join("/__thumb/", c_path) + " It is a folder",
+				"file": path.Join("/__thumb/", c_path) + " It is not a regular file",
 				"msg":  "It is a folder",
 			}, "application/json")
 		}
@@ -92,10 +97,10 @@ func GetThumb(c *fiber.Ctx) error {
 	}
 
 	file_ext := util.GetExtNorm(c_path)
-	orig_filename := util.GetFileName(c_path)
+	//orig_filename := util.GetFileName(c_path)
 
-	is_match, _ := regexp.MatchString("^(jpg|jpeg|png|gif|pdf|rtf|doc|docx|xls|xlsx|odt|ods)$", file_ext)
-	if !is_match {
+	is_preview_match := previewAllRegex.MatchString(file_ext)
+	if !is_preview_match {
 
 		model2.EventLogAdd(c, 500, "GetThumb", path.Join("/__thumb/", c_path)+" Only for JPEG, PNG, GIF and office files")
 
@@ -108,11 +113,8 @@ func GetThumb(c *fiber.Ctx) error {
 
 	// ------------------------------------------------------------------------------------------------------------------------------
 
-	//hash_name := md5.Sum([]byte(orig_filename + modtime_human + size_human + c_width))
-	//hex_name := hex.EncodeToString(hash_name[:])
 	hex_name := ""
 
-	//if db != nil {
 	if model2.Dbse.BadgerEnable {
 
 		/*
@@ -142,6 +144,16 @@ func GetThumb(c *fiber.Ctx) error {
 		*/
 	}
 
+	if fileInfoOriginal.Size() >= 20*1024*1024 {
+
+		model2.EventLogAdd(c, 500, "GetThumb", path.Join("/__thumb/", c_path)+", original file size is more than 20 Megabytes")
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": 500,
+			"file": path.Join("/__thumb/", c_path),
+		}, "application/json")
+	}
+
 	hex_name = util.GetMd5File(path.Join(conf.ArgFold, c_path))
 
 	if len(hex_name) == 0 {
@@ -154,262 +166,32 @@ func GetThumb(c *fiber.Ctx) error {
 		}, "application/json")
 	}
 
-	// ------------------------------------------------------------------------------------------------------------------------------
+	if _, err := os.Stat(path.Join(conf.ConfigRoot, "thumb", hex_name)); err == nil {
 
-	is_img_match, _ := regexp.MatchString("^(jpg|jpeg|png|gif)$", file_ext)
+		model2.EventLogAdd(c, 200, "GetThumb", "SendFile thumb/cache "+path.Join(c_path))
 
-	if is_img_match {
-
-		if _, err := os.Stat(path.Join(conf.ConfigRoot, "thumb", hex_name)); err == nil {
-
-			model2.EventLogAdd(c, 200, "GetThumb", "SendFile thumb/cache "+path.Join(c_path))
-
-			return c.SendFile(path.Join(conf.ConfigRoot, "thumb", hex_name), false)
-
-		} else if errors.Is(err, os.ErrNotExist) {
-
-			// ------------------------------------------------------------------------------------------------------------------------------
-
-			if fileInfo.Size() < 50*1024 {
-
-				model2.EventLogAdd(c, 200, "GetThumb", "SendFile orig without resize "+path.Join(c_path)+", size is small")
-
-				return c.SendFile(path.Join(conf.ArgFold, c_path), false)
-			}
-
-			// ------------------------------------------------------------------------------------------------------------------------------
-
-			input, _ := os.Open(path.Join(conf.ArgFold, c_path))
-			defer input.Close()
-
-			output, _ := os.Create(path.Join(conf.ConfigRoot, "thumb", hex_name))
-			defer output.Close()
-
-			var src image.Image
-
-			// Decode the image (from PNG to image.Image):
-			if file_ext == "png" {
-				src, err = png.Decode(input)
-				if err != nil {
-					panic(err)
-				}
-			}
-
-			if file_ext == "jpg" {
-
-				// src, err = jpeg.Decode(input)
-				src, _, err = exiffix.Decode(input)
-				if err != nil {
-					panic(err)
-				}
-			}
-
-			if file_ext == "gif" {
-				src, err = gif.Decode(input)
-				if err != nil {
-					panic(err)
-				}
-			}
-
-			ratio := (float64)(src.Bounds().Max.Y) / (float64)(src.Bounds().Max.X)
-			i_height := int(math.Round(float64(i_width) * ratio))
-
-			var dst *image.RGBA
-
-			if src.Bounds().Max.X > i_width || src.Bounds().Max.Y > i_height {
-				dst = image.NewRGBA(image.Rect(0, 0, i_width, i_height))
-			} else {
-
-				model2.EventLogAdd(c, 200, "GetThumb", "SendFile orig without resize "+path.Join(conf.ArgFold, c_path))
-
-				err := os.Remove(path.Join(conf.ConfigRoot, "thumb", hex_name))
-				if err != nil {
-					panic(err)
-				}
-
-				return c.SendFile(path.Join(conf.ArgFold, c_path), false)
-			}
-
-			// Resize:
-			draw.NearestNeighbor.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
-
-			if file_ext == "png" {
-				err = png.Encode(output, dst)
-				if err != nil {
-					panic(err)
-				}
-			}
-
-			if file_ext == "jpg" {
-				err = jpeg.Encode(output, dst, nil)
-				if err != nil {
-					panic(err)
-				}
-			}
-
-			if file_ext == "gif" {
-				err = gif.Encode(output, dst, nil)
-				if err != nil {
-					panic(err)
-				}
-			}
-
-			output.Close()
-			input.Close()
-
-			src = nil
-			dst = nil
-
-			model2.EventLogAdd(c, 200, "GetThumb", "Resize and SendFile "+path.Join(c_path))
-
-			return c.SendFile(path.Join(conf.ConfigRoot, "thumb", hex_name), false)
-		}
+		return c.SendFile(path.Join(conf.ConfigRoot, "thumb", hex_name), false)
 
 	}
 
-	is_office_match, _ := regexp.MatchString("^(pdf|rtf|doc|docx|xls|xlsx|odt|ods)$", file_ext)
+	// ------------------------------------------------------------------------------------------------------------------------------
 
-	if is_office_match {
+	is_preview_img := previewImgRegex.MatchString(file_ext)
+	if is_preview_img {
 
-		if runtime.GOOS == "windows" {
+		if fileInfoOriginal.Size() < 50*1024 {
 
-			_, err := os.Stat("C:\\Program Files\\LibreOffice\\program\\soffice.exe")
-			if err != nil {
-
-				model2.EventLogAdd(c, 500, "GetThumb", "Error libreoffice not found "+err.Error())
-
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-					"code": 500,
-					"msg":  "Error libreoffice not found",
-				}, "application/json")
-			}
-
-		} else {
-
-			_, err := exec.Command("bash", "-c", "libreoffice --help").Output()
-			if err != nil {
-
-				model2.EventLogAdd(c, 500, "GetThumb", "Error libreoffice not found "+err.Error())
-
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-					"code": 500,
-					"msg":  "Error libreoffice not found",
-				}, "application/json")
-			}
+			model2.EventLogAdd(c, 200, "GetThumb", "SendFile orig file without resize "+path.Join(c_path)+", size is small")
+			return c.SendFile(path.Join(conf.ArgFold, c_path), false)
 		}
 
-		// --------------------------------------------------------------------------------------------------------------------------------
+		return ServeImgFile(c, c_path, hex_name)
+	}
 
-		var FI os.FileInfo
+	is_preview_doc := previewDocRegex.MatchString(file_ext)
+	if is_preview_doc {
 
-		if FI, err = os.Stat(path.Join(conf.ConfigRoot, "thumb", hex_name)); err != nil {
-
-			//panic("Stat error " + path.Join(conf.ConfigRoot, "thumb", hex_name) + " " + err.Error())
-		}
-
-		if FI != nil {
-
-			if FI.Size() == 0 {
-
-				if err = os.Remove(path.Join(conf.ConfigRoot, "thumb", hex_name)); err != nil {
-					panic("Problem of remove zero file " + path.Join(conf.ConfigRoot, "thumb", hex_name) + " " + err.Error())
-				}
-
-			} else {
-
-				model2.EventLogAdd(c, 200, "GetThumb", "SendFile thumb/cache "+path.Join(c_path))
-
-				return c.SendFile(path.Join(conf.ConfigRoot, "thumb", hex_name), false)
-			}
-		}
-
-		// --------------------------------------------------------------------------------------------------------------------------------
-
-		filepath_tmp := path.Join(conf.ConfigRoot, "temp")
-
-		if runtime.GOOS == "windows" {
-			filepath_tmp = util.RotateSlash(filepath_tmp)
-		}
-
-		var readerFile *os.File
-
-		read_err_cnt := 1
-		read_err := errors.New("read error 99")
-
-		for read_err != nil {
-
-			if runtime.GOOS == "windows" {
-
-				arg_fold_path := util.RotateSlash(path.Join(conf.ArgFold, c_path))
-
-				util.RunAnyCommandUnderWin(`"C:/Program Files/LibreOffice/program/soffice.exe" --headless --norestore --nologo --convert-to png --outdir "` + filepath_tmp + `" "` + arg_fold_path + `"`)
-
-			} else {
-
-				cmd := exec.Command("bash", "-c", "libreoffice --headless --norestore --nologo --convert-to png --outdir "+filepath_tmp+" \""+path.Join(conf.ArgFold, c_path)+"\"")
-				cmd.Dir = conf.ArgFold
-				//out, _ := cmd.Output()
-				//fmt.Println("out=", out)
-
-				stderr, _ := cmd.StderrPipe()
-				if err := cmd.Start(); err != nil {
-					panic(err)
-				}
-
-				scanner := bufio.NewScanner(stderr)
-				for scanner.Scan() {
-					fmt.Println("libreoffice:", scanner.Text())
-				}
-
-			}
-
-			readerFile, read_err = os.Open(path.Join(filepath_tmp, orig_filename+".png"))
-			if read_err != nil {
-
-				model2.EventLogAdd(c, 500, "GetThumb", "REPEAT Error libreoffice, open file "+read_err.Error())
-
-				time.Sleep(2 * time.Second)
-			}
-
-			if read_err_cnt > 5 {
-
-				model2.EventLogAdd(c, 500, "GetThumb", "SEVERAL Errors libreoffice, open file "+read_err.Error())
-
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-					"code": 500,
-					"msg":  "Error libreoffice",
-				}, "application/json")
-			}
-
-			read_err_cnt++
-		}
-
-		output, _ := os.Create(path.Join(conf.ConfigRoot, "thumb", hex_name))
-
-		_, err = io.Copy(output, readerFile)
-		if err != nil {
-
-			model2.EventLogAdd(c, 500, "GetThumb", "Error copy after libreoffice convert: "+err.Error())
-
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"code": 500,
-				"msg":  "Error copy",
-			}, "application/json")
-
-		}
-		output.Close()
-		readerFile.Close()
-
-		if _, err = os.Stat(path.Join(filepath_tmp, orig_filename+".png")); err == nil {
-
-			os.Remove(path.Join(filepath_tmp, orig_filename+".png"))
-		}
-
-		// --------------------------------------------------------------------------------------------------------------------------------
-
-		model2.EventLogAdd(c, 200, "GetThumb", "Make office thumbnail and SendFile "+path.Join(c_path))
-
-		return c.SendFile(path.Join(conf.ConfigRoot, "thumb", hex_name), false)
+		return ServeOfficeFile(c, c_path, hex_name)
 	}
 
 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -418,4 +200,246 @@ func GetThumb(c *fiber.Ctx) error {
 		"msg":  "Bad request",
 	}, "application/json")
 
+}
+
+func ServeImgFile(c *fiber.Ctx, c_path string, hex_name string) error {
+
+	file_ext := util.GetExtNorm(c_path)
+	var err error
+
+	// ------------------------------------------------------------------------------------------------------------------------------
+
+	input, _ := os.Open(path.Join(conf.ArgFold, c_path))
+	defer input.Close()
+
+	output, _ := os.Create(path.Join(conf.ConfigRoot, "thumb", hex_name))
+	defer output.Close()
+
+	var src image.Image
+
+	// Decode the image (from PNG to image.Image):
+	if file_ext == "png" {
+		src, err = png.Decode(input)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if file_ext == "jpg" {
+
+		// src, err = jpeg.Decode(input)
+		src, _, err = exiffix.Decode(input)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if file_ext == "gif" {
+		src, err = gif.Decode(input)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	i_width := 600
+
+	ratio := (float64)(src.Bounds().Max.Y) / (float64)(src.Bounds().Max.X)
+	i_height := int(math.Round(float64(i_width) * ratio))
+
+	var dst *image.RGBA
+
+	if src.Bounds().Max.X > i_width || src.Bounds().Max.Y > i_height {
+		dst = image.NewRGBA(image.Rect(0, 0, i_width, i_height))
+	} else {
+
+		model2.EventLogAdd(c, 200, "ServeImgFile", "SendFile orig without resize "+path.Join(conf.ArgFold, c_path))
+
+		err := os.Remove(path.Join(conf.ConfigRoot, "thumb", hex_name))
+		if err != nil {
+			panic(err)
+		}
+
+		return c.SendFile(path.Join(conf.ArgFold, c_path), false)
+	}
+
+	// Resize:
+	draw.NearestNeighbor.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
+
+	if file_ext == "png" {
+		err = png.Encode(output, dst)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if file_ext == "jpg" {
+		err = jpeg.Encode(output, dst, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if file_ext == "gif" {
+		err = gif.Encode(output, dst, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	output.Close()
+	input.Close()
+
+	src = nil
+	dst = nil
+
+	model2.EventLogAdd(c, 200, "ServeImgFile", "Resize and SendFile "+path.Join(c_path))
+
+	return c.SendFile(path.Join(conf.ConfigRoot, "thumb", hex_name), false)
+
+}
+
+func ServeOfficeFile(c *fiber.Ctx, c_path string, hex_name string) error {
+
+	orig_filename := util.GetFileName(c_path)
+	var err error
+
+	if runtime.GOOS == "windows" {
+
+		_, err := os.Stat("C:\\Program Files\\LibreOffice\\program\\soffice.exe")
+		if err != nil {
+
+			model2.EventLogAdd(c, 500, "ServeOfficeFile", "Error libreoffice not found "+err.Error())
+
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"code": 500,
+				"msg":  "Error libreoffice not found",
+			}, "application/json")
+		}
+
+	} else {
+
+		_, err := exec.Command("bash", "-c", "libreoffice --help").Output()
+		if err != nil {
+
+			model2.EventLogAdd(c, 500, "ServeOfficeFile", "Error libreoffice not found "+err.Error())
+
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"code": 500,
+				"msg":  "Error libreoffice not found",
+			}, "application/json")
+		}
+	}
+
+	// --------------------------------------------------------------------------------------------------------------------------------
+
+	var FI os.FileInfo
+
+	if FI, err = os.Stat(path.Join(conf.ConfigRoot, "thumb", hex_name)); err != nil {
+
+		//panic("Stat error " + path.Join(conf.ConfigRoot, "thumb", hex_name) + " " + err.Error())
+	}
+
+	if FI != nil {
+
+		if FI.Size() == 0 {
+
+			if err = os.Remove(path.Join(conf.ConfigRoot, "thumb", hex_name)); err != nil {
+				panic("Problem of remove zero file " + path.Join(conf.ConfigRoot, "thumb", hex_name) + " " + err.Error())
+			}
+
+		} else {
+
+			model2.EventLogAdd(c, 200, "ServeOfficeFile", "SendFile thumb/cache "+path.Join(c_path))
+
+			return c.SendFile(path.Join(conf.ConfigRoot, "thumb", hex_name), false)
+		}
+	}
+
+	// --------------------------------------------------------------------------------------------------------------------------------
+
+	filepath_tmp := path.Join(conf.ConfigRoot, "temp")
+
+	if runtime.GOOS == "windows" {
+		filepath_tmp = util.RotateSlash(filepath_tmp)
+	}
+
+	var readerFile *os.File
+
+	read_err_cnt := 1
+	read_err := errors.New("read error 99")
+
+	for read_err != nil {
+
+		if runtime.GOOS == "windows" {
+
+			arg_fold_path := util.RotateSlash(path.Join(conf.ArgFold, c_path))
+
+			util.RunAnyCommandUnderWin(`"C:/Program Files/LibreOffice/program/soffice.exe" --headless --norestore --nologo --convert-to png --outdir "` + filepath_tmp + `" "` + arg_fold_path + `"`)
+
+		} else {
+
+			cmd := exec.Command("bash", "-c", "libreoffice --headless --norestore --nologo --convert-to png --outdir "+filepath_tmp+" \""+path.Join(conf.ArgFold, c_path)+"\"")
+			cmd.Dir = conf.ArgFold
+			//out, _ := cmd.Output()
+			//fmt.Println("out=", out)
+
+			stderr, _ := cmd.StderrPipe()
+			if err := cmd.Start(); err != nil {
+				panic(err)
+			}
+
+			scanner := bufio.NewScanner(stderr)
+			for scanner.Scan() {
+				fmt.Println("libreoffice:", scanner.Text())
+			}
+
+		}
+
+		readerFile, read_err = os.Open(path.Join(filepath_tmp, orig_filename+".png"))
+		if read_err != nil {
+
+			model2.EventLogAdd(c, 500, "ServeOfficeFile", "REPEAT Error libreoffice, open file "+read_err.Error())
+
+			time.Sleep(2 * time.Second)
+		}
+
+		if read_err_cnt > 5 {
+
+			model2.EventLogAdd(c, 500, "ServeOfficeFile", "SEVERAL Errors libreoffice, open file "+read_err.Error())
+
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"code": 500,
+				"msg":  "Error libreoffice",
+			}, "application/json")
+		}
+
+		read_err_cnt++
+	}
+
+	output, _ := os.Create(path.Join(conf.ConfigRoot, "thumb", hex_name))
+
+	_, err = io.Copy(output, readerFile)
+	if err != nil {
+
+		model2.EventLogAdd(c, 500, "ServeOfficeFile", "Error copy after libreoffice convert: "+err.Error())
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": 500,
+			"msg":  "Error copy",
+		}, "application/json")
+
+	}
+	output.Close()
+	readerFile.Close()
+
+	if _, err = os.Stat(path.Join(filepath_tmp, orig_filename+".png")); err == nil {
+
+		os.Remove(path.Join(filepath_tmp, orig_filename+".png"))
+	}
+
+	// --------------------------------------------------------------------------------------------------------------------------------
+
+	model2.EventLogAdd(c, 200, "ServeOfficeFile", "Make office thumbnail and SendFile "+path.Join(c_path))
+
+	return c.SendFile(path.Join(conf.ConfigRoot, "thumb", hex_name), false)
 }
