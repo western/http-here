a9dff2bc225837f86a39dae8ce3d9e0f51d675d4
opensuse156 <westroads@gmail.com>
2026-03-08 08:06:34 +0300

    modified:   internal/model2/user.go

--- internal/model2/user.go
+++ internal/model2/user.go
@@ -353,25 +353,27 @@ func UserList() {
 
 		fmt.Println(t.Render())
 
-		Dbse.Badger.View(func(txn *badger.Txn) error {
-			it := txn.NewIterator(badger.DefaultIteratorOptions)
-			defer it.Close()
-			prefix := []byte("idx_user_login_")
-			for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
-				item := it.Item()
-				k := item.Key()
-				err := item.Value(func(v []byte) error {
+		/*
+			Dbse.Badger.View(func(txn *badger.Txn) error {
+				it := txn.NewIterator(badger.DefaultIteratorOptions)
+				defer it.Close()
+				prefix := []byte("idx_user_login_")
+				for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+					item := it.Item()
+					k := item.Key()
+					err := item.Value(func(v []byte) error {
 
-					fmt.Printf("key=%s, value=%s\n", k, v)
+						fmt.Printf("key=%s, value=%s\n", k, v)
 
-					return nil
-				})
-				if err != nil {
-					return err
+						return nil
+					})
+					if err != nil {
+						return err
+					}
 				}
-			}
-			return nil
-		})
+				return nil
+			})
+		*/
 
 	}
 }
